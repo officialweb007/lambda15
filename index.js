402e54a83a8f43883cc1cb6f2f6491ff61013d1d
officialweb007 <sunil@codemymobile.com>
2022-03-24 15:05:48 +0530

    example

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 exports.handler = async(event, context) => {
-    return event.firstName
+
+    date1 = new Date("06/30/2019");
+    date2 = new Date("07/30/2019");
+
+    // To calculate the time difference of two dates
+    Difference_In_Time = date2.getTime() - date1.getTime();
+
+    // To calculate the no. of days between two dates
+    Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+
+    console.log(`"${Difference_In_Days}`);
 }
